87aaa7cc31affbc502fcd67e1237cd339ab8e2a9
FishMinus <47413312+FishMinus@users.noreply.github.com>
2022-03-14 21:48:44 +0100

    New page + better reading

--- reader/read.js
+++ reader/read.js
@@ -1,6 +1,8 @@
 const MainImage = document.getElementById("MainImage");
 var currentPage = 1;
+var lastPagenumber = 0;
 var currentRead = "kanin";
+var OnLastPage = false;
 function imageset(com, page){
     switch(com){
         case "add":
@@ -15,6 +17,19 @@ function imageset(com, page){
     }
     if (currentPage <= 0) currentPage = 1;
     MainImage.src = `reader/comics/${currentRead}/${currentRead}_${currentPage}.png`;
+    if (!OnLastPage) lastPagenumber = Number(String(MainImage.src.split(currentRead+"_")[1]).split(".png")[0])-1;
+}
+MainImage.onerror = function() {
+    console.log("No more pages/page failed to load")
+    MainImage.src = `reader/end.png`;
+    currentPage = lastPagenumber+1;
+    OnLastPage = true;
+    /*currently this cant be set back to false, but it 
+    works probably as a check for the last page, which then uses that 
+    value for the rest of the session, idk if thats good enough though
+    
+    I checked, thats xcsatcly what it does, pretty cool lol
+    */
 }
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     MainImage.style.width = "80%"
